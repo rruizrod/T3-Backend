d12a45b4f48c32182bed4a09bc8442863dabbc1d
Ricardo Ruiz <kxngvenom@gmail.com>
2021-03-05 16:59:22 -0500

    SETUP [ADD] Boilerplate app.js [ADD] Boilerplate index.js [ADD] Middleware/Logger in utils

---
+++ app.js
@@ -0,0 +1,31 @@
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+//logger.info('Connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+//  --  ADD ROUTERS HERE FOR DIFFERENT ENDPOINTS ---
+app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
