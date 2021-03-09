4ada61e98b04a024fcae666b3ce1ce97b70a7349
Ricardo Ruiz <kxngvenom@gmail.com>
2021-03-09 01:40:16 -0500

    Added servers field for SwaggerUI

--- utils/docs.js
+++ utils/docs.js
@@ -3,12 +3,12 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerOptions = {
   definition: {
     openapi: '3.0.1',
-    //components: {},
     info: {
       title: 'T3 KnightHacks Backend API',
       version: '1.0.0',
       description: 'Social Media Backend API'
-    }
+    },
+    servers: [{url: 't3-dev.rruiz.dev/'}]
   },
   apis: ['./controllers/*.js']
 };
