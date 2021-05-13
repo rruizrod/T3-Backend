aeb98f5c5808cb6c9a5fa990016ccd285bb09dec
Leo Zhang <leotheasianlion@gmail.com>
2021-05-12 20:07:57 -0400

    update
    Users should now be able to send messages back and forth and the socket will update the list of messages to be received accordingly for each user signed into the socket server

--- servers/socket_io.js
+++ servers/socket_io.js
@@ -6,6 +6,11 @@ const io = require('socket.io')(server)
 var clients = {}
 var lstOfMsgs = []
 
+// client id as keys and list of messages as values
+// the lists will be filled with pending messages
+// for the users of the corresponding id to receive once the user has signed in
+var clientMessages = {}
+
 // middleware
 app.use(express.json())
 io.on("connection", socket =>
@@ -20,36 +25,58 @@ io.on("connection", socket =>
     clients[id] = socket
     // console.log(clients);
 
+    // if there were messages previous that need to be carried
+    // over, do so now
+    if (clientMessages[id])
+    {
+      clientMessages[id] = [...clientMessages[id]]
+    }
+    else
+    {
+      clientMessages[id] = []
+    }
+
     // load contents of array into user's chat list on the front end
-    if (lstOfMsgs.length > 0)
+    if (clientMessages[id].length > 0)
     {
       console.log(`loading messages to user ${id}`)
-      clients[id].emit('message',lstOfMsgs)
-      lstOfMsgs.splice(0, lstOfMsgs.length)
+      clients[id].emit('message',clientMessages[id])
+
+      // clear array
+      clientMessages[id].splice(0, clientMessages[id].length)
     }
   })
 
   // message from sender
   socket.on('message', (msg) => {
-    console.log(`type of msg is ${typeof(msg)}`)
+    // console.log(`type of msg is ${typeof(msg)}`)
     let targetId = msg.targetId
     // clients[targetId] = socket
 
-    // maintain a global list of msg objects for the receiver to
+    // null check
+    if (!clientMessages[targetId])
+    {
+      clientMessages[targetId] = []
+    }
+
+    // maintain list of msg objects for the receiver to
     // receive once he/she is signed in (i.e. enters the dm chat)
-    lstOfMsgs.push(msg)
+    clientMessages[targetId].push(msg)
 
     // null check before sending to target
     if (clients[targetId])
     {
-      clients[targetId].emit('message',lstOfMsgs)
+      // sends off messages and assumes the front-end saves it in its own way
+      clients[targetId].emit('message', clientMessages[targetId])
       console.log(`sending message to user ${targetId}`)
 
       // clear array
-      lstOfMsgs.splice(0, lstOfMsgs.length)
+      clientMessages[targetId].splice(0, clientMessages[targetId].length)
     }
-
-    else { console.log('client is null'); }
+    else
+    {
+      console.log('receiver most likely isn\'t signed in')
+    }
 
   })
 
