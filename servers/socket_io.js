2101db4315723ec34fa66e317bed72089a70add0
Leo Zhang <leotheasianlion@gmail.com>
2021-05-18 17:06:27 -0400

    major updates
    messages are now properly filtered when receiving them from the correct match

--- servers/socket_io.js
+++ servers/socket_io.js
@@ -5,12 +5,12 @@ const port = process.env.PORT || 5000
 const io = require('socket.io')(server)
 var mongoose = require('mongoose')
 require('dotenv').config()
-var clients = {}
+var signedInClients = {}
 
-
-const uri = `mongodb+srv://leozhang1:${process.env.PASSWORD}@cluster0.pti3a.mongodb.net/myFirstDatabase?retryWrites=true`;
 
 //#region mongoose init
+const uri = `mongodb+srv://leozhang1:${process.env.PASSWORD}@cluster0.pti3a.mongodb.net/myFirstDatabase?retryWrites=true`;
+
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false, }, (err) =>
 {
   if (err) throw err
@@ -23,24 +23,25 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCrea
 // imported backlog model
 const Backlog = require('../models/backlog')
 
-var chatSchema = new mongoose.Schema({
-  messages: Array,
-  userId: String,
-})
+// var chatSchema = new mongoose.Schema({
+//   messages: Array,
+//   userId: String,
+// })
 
-var chat = mongoose.model('Message', chatSchema)
+// var chat = mongoose.model('Message', chatSchema)
 //#endregion
 
 const getPending = async (id) => {
   const currBacklog = await Backlog.findOne({uid: id})
 
-  return currBacklog ? currBacklog.messages : null
+  return currBacklog
 }
 
-const clearMessages = async (id) => {
+// todo only clear the messages that have been filtered out DONE
+const clearMessages = async (id, leftOvers) => {
   const currBacklog = await Backlog.findOne({uid: id})
   if (currBacklog == null) return
-  currBacklog.messages = []
+  currBacklog.messages = leftOvers
   currBacklog.save()
 }
 
@@ -56,10 +57,11 @@ const createUserBackLog = async (id, message) => {
     return
   }
 
-  // console.log(backlog);
+  // console.log(backlog)
+  // IMPORTANT: it is imperative that the key fields you want the db to recognize and store to match those of the back log schema
   backlog.messages.push(message)
   backlog.save()
-  console.log('saved message')
+  console.log('saved new message')
 }
 
 /*
@@ -67,7 +69,7 @@ TODO
 add a sender ID (Specify on connection)
 i.e. handle case when receiver isn't signed in and is signed in
 Send just messages from sourceID and clear just those messages
-messages.filter((m) => m.senderId === senderId)
+messages.filter((m) => m.senderID === senderID)
 */
 
 // middleware
@@ -78,45 +80,76 @@ io.on("connection", socket =>
   console.log(socket.id, 'has joined')
 
   // receive from frontend client (socket.on() ...)
-  // socket.on('/test', (msg) => console.log(msg))
-  socket.on('signin', async (id) =>
+
+  // todo add otherUser id as a param as well DONE
+  socket.on('signin', async (activeUserId, otherUserId) =>
   {
-    console.log(`${id} has signed in to dm chat`)
-    clients[id] = socket
+    console.log(`${activeUserId} has signed in to dm chat with ${otherUserId}`)
+    signedInClients[activeUserId] = socket
 
-    const messages = await getPending(id)
-    console.log(`messages: ${messages}`)
-    console.log(`data type of messages: ${typeof(messages)}`)
-    clients[id].emit('message', messages)
-    // for(var msg of messages){
-    //   //clients[id].emit('message', [msg.message])
-    //   console.log(msg)
-    // }
+    const getBackLog = await getPending(activeUserId)
+    console.log(`backlog data: ${getBackLog}`);
+    var messages = getBackLog ? getBackLog.messages : {}
+    messages = Object.values(messages);
+    // console.log(`data type of messages: ${typeof(messages)}`)
+    console.log(`messages: ${messages}`);
 
-    clearMessages(id)
+    // todo filter messages variable based on otherUser id
+    const lstToSendToFrontEnd = messages.filter((m) => m.senderID === otherUserId)
+    console.log(`messages to send to front end: ${lstToSendToFrontEnd}`)
+
+    socket.emit('message', lstToSendToFrontEnd)
+
+    const leftOvers = messages.filter((m) => !lstToSendToFrontEnd.includes(m))
+    console.log(`what's not being sent to frontend: ${leftOvers}`)
+
+    clearMessages(activeUserId, leftOvers)
   })
 
   // message from sender
   socket.on('message', (msg) =>
   {
-    // format of every msg arg: {'message': message, 'sourceId': sourceId, 'targetId': targetId}
+    // format of every msg arg: {'message': message, 'senderID': senderID, 'targetId': targetId}
 
     let receiverId = msg.targetId
 
-    if(!clients[receiverId])
+    //#region msg debugger
+    // Object.keys(msg).forEach(key => {
+    //   console.log(key, msg[key]);
+    // })
+    //#endregion
+
+    // if they're in the clients obj, then they're
+    // signed in. Otherwise, they're not
+    if(!signedInClients[receiverId])
     {
+      //#region IMPORTANT NOTE
+
+      /**
+       * msg object key names must match those of the backlog shown below inorder to be recognized and stored in the database
+       * messages: [
+          {
+            id: Number,
+            message: String,
+            senderID: String,
+            timeStamp: String
+          }
+        ]
+       */
+
+      //#endregion
       createUserBackLog(receiverId, msg)
     }
     else
     {
-      clients[id].emit('message', [msg])
+      socket.emit('message', [msg])
     }
   })
 
   socket.on('signedout', (id) =>
   {
     console.log(`${id} has signed out of dm chat`)
-    delete clients[id]
+    delete signedInClients[id]
     // console.log(clients);
   })
 })
